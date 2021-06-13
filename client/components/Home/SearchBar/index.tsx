6506ee7243c0ede934e809615235a5d62898adfa
Nerio Ramirez <nerioramirez17@gmail.com>
2021-06-13 15:51:52 -0300

    ewf

--- client/components/Home/SearchBar/index.tsx
+++ client/components/Home/SearchBar/index.tsx
@@ -13,6 +13,8 @@ const SearchBox = () => {
   const [destino, setdestino] = useState(null);
   const [date, setDate] = useState({ from: undefined, to: undefined });
   const [guest, setguest] = useState({ adults: 0, kids: 0, pets: 0 });
+
+  asd;
   const router = useRouter();
 
   const handleSearch = (e) => {
