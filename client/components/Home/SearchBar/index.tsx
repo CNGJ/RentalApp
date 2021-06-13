02af620e5541721b04fcbbeaaf818b3fba13ca34
Nerio Ramirez <nerioramirez17@gmail.com>
2021-06-13 15:39:25 -0300

    probando

--- client/components/Home/SearchBar/index.tsx
+++ client/components/Home/SearchBar/index.tsx
@@ -9,7 +9,6 @@ import AutoComplete from '../../../lib/AutoComplete';
 import { DatePickerRange } from '../../../lib/DatePickerRange';
 import { InputGuests } from '../../../lib/Guestinput';
 
-sdf;
 const SearchBox = () => {
   const [destino, setdestino] = useState(null);
   const [date, setDate] = useState({ from: undefined, to: undefined });
