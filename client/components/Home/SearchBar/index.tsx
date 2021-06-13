341aa35c1377c2e3e69486553ec67b1c14f49066
Nerio Ramirez <nerioramirez17@gmail.com>
2021-06-13 15:52:53 -0300

    probando

--- client/components/Home/SearchBar/index.tsx
+++ client/components/Home/SearchBar/index.tsx
@@ -13,7 +13,7 @@ const SearchBox = () => {
   const [destino, setdestino] = useState(null);
   const [date, setDate] = useState({ from: undefined, to: undefined });
   const [guest, setguest] = useState({ adults: 0, kids: 0, pets: 0 });
-
+  sdf;
   const router = useRouter();
 
   const handleSearch = (e) => {
